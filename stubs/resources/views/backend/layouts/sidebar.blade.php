9d67e2314153f2f871e4b0d69d79db1fe140bd6a
Rabeea <rabieali520@gmail.com>
2021-02-10 16:26:06 +0300

    wip

--- stubs/resources/views/backend/layouts/sidebar.blade.php
+++ stubs/resources/views/backend/layouts/sidebar.blade.php
@@ -31,5 +31,13 @@
             <span class="mx-3">Dashboard</span>
         </a>
     </div>
+    <a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+        href="#">
+        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+        <span class="mx-3">Users</span>
+    </a>
 </div>
 <!-- end sidebar -->
